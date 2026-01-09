d27c08661bdbde29eefe54c0ed63b5a2220095e1
ishansharma <ishankaul990@gmail.com>
2026-01-08 23:37:53 -0800

    Calculator mini project

--- src/com/company/CalculatorProject/Main.java
+++ src/com/company/CalculatorProject/Main.java
@@ -3,7 +3,8 @@ package com.company.CalculatorProject;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.add(2,4);
+       double sum =  calculator.add(2,4);
+        System.out.println(sum);
         System.out.println(calculator.divide(4,2));
     }
 }
