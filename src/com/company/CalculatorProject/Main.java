af672aee594609e6bb1ad844bb8df835c9ca81d2
ishansharma <ishankaul990@gmail.com>
2026-01-08 23:31:37 -0800

    Calculator mini project

---
+++ src/com/company/CalculatorProject/Main.java
@@ -0,0 +1,9 @@
+package com.company.CalculatorProject;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add(2,4);
+        System.out.println(calculator.divide(4,2));
+    }
+}
